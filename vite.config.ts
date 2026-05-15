045deaae66e943f352d495c38c951306c3f53a43
makinde olasubomi <121644515+Shubbin@users.noreply.github.com>
2026-05-16 00:36:50 +0100

    Fix: Cleaned up package.json dependencies and reverted vite import

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { tanstackStartVite } from "@tanstack/react-start/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [
-    TanStackRouterVite(),
+    tanstackStartVite(),
     react(),
     tailwindcss(),
     tsconfigPaths(),
-    tanstackStartVite({
-      server: {
-        entry: "src/server.ts",
-      },
-    }),
   ],
 });
